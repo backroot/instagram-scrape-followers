00d0d987e6f01bbd67f8eefc74ed9dca906b3d33
backroot <kotaro_saito@backroot.net>
2019-10-08 07:17:30 +0900

    fix

--- scrape.py
+++ scrape.py
@@ -15,11 +15,21 @@ import configparser
 import time
 import random
 import sys
+import json
+import re
+import urllib.parse
 
 def main():
 
+    if len(sys.argv) <= 2:
+        print("第１引数に username、第２引数に query_hash を指定してください。")
+        sys.exit(1)
+
     username = sys.argv[1]
+    query_hash = sys.argv[2]
+
     print("start crawling follower of " + username)
+    print("query hash is " + query_hash)
 
     options = Options()
     options.add_argument('--headless')
@@ -44,40 +54,65 @@ def main():
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "Y8-fY")))
     follower_button = driver.find_elements_by_css_selector("li.Y8-fY")[1]
     follower_button.click()
-    time.sleep(10)
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "isgrP")))
 
-    # You can get followers by scrolling the follower popup window.
-    height = driver.execute_script("return document.querySelectorAll('div.isgrP')[0].scrollHeight;")
-    i = 0
-    before_top = 0
-    while True:
-        i += 100
-        try: 
-            driver.execute_script("document.querySelectorAll('div.isgrP')[0].scrollTo(0, " + str(i) + ");")
-            time.sleep(0.5)
-            height = driver.execute_script("return document.querySelectorAll('div.isgrP')[0].scrollHeight;")
-            top = driver.execute_script("return document.querySelectorAll('div.isgrP')[0].scrollTop;")
-            print("\rfollowers window scrolling... {0}/{1}".format(top, height), end='', flush=True)
-            if before_top == top:
-                break
-            before_top = top
-        except TimeoutException as ex:
-            print("Exception has been thrown. " + str(ex))
-            break
+    user_id = driver.execute_script("return _sharedData['entry_data']['ProfilePage'][0]['graphql']['user']['id']")
+    print("user id is " + user_id)
 
-    print("")
-    page_url = driver.page_source
-    soup = bs4.BeautifulSoup(page_url,"lxml")
-    elm = soup.find_all("a", {"class": "FPmhX notranslate _0imsa"})
-    followers = []
-    for e in elm:
-        followers.append(e.text)
-        print(e.text)
+    source = driver.page_source
 
-    df = pd.Series(followers)
-    df.index = df.index + 1
-    df.to_csv("insta_followers_of_" + username + ".csv", header=False)
-    print("csv file is created.")
+    source = source.rstrip()
+    #js = re.findall(r'Consumer\.js', source)
+
+    print(source)
+
+    query = {
+        "query_hash": query_hash,
+        "variables": {
+            "id": user_id,
+            "include_reel": "true",
+            "fetch_mutual": "false",
+            "first": 12
+        }
+    }
+    query = urllib.parse.quote(json.dumps(query))
+
+
+    print(query)
+    #driver.get('https://www.instagram.com/graphql/query/?' + query)
+
+#    # You can get followers by scrolling the follower popup window.
+#    height = driver.execute_script("return document.querySelectorAll('div.isgrP')[0].scrollHeight;")
+#    i = 0
+#    before_top = 0
+#    while True:
+#        i += 100
+#        try: 
+#            driver.execute_script("document.querySelectorAll('div.isgrP')[0].scrollTo(0, " + str(i) + ");")
+#            time.sleep(0.5)
+#            height = driver.execute_script("return document.querySelectorAll('div.isgrP')[0].scrollHeight;")
+#            top = driver.execute_script("return document.querySelectorAll('div.isgrP')[0].scrollTop;")
+#            print("\rfollowers window scrolling... {0}/{1}".format(top, height), end='', flush=True)
+#            if before_top == top:
+#                break
+#            before_top = top
+#        except TimeoutException as ex:
+#            print("Exception has been thrown. " + str(ex))
+#            break
+
+    #print("")
+    #page_url = driver.page_source
+    #soup = bs4.BeautifulSoup(page_url,"lxml")
+    #elm = soup.find_all("a", {"class": "FPmhX notranslate _0imsa"})
+    #followers = []
+    #for e in elm:
+    #    followers.append(e.text)
+    #    print(e.text)
+
+    #df = pd.Series(followers)
+    #df.index = df.index + 1
+    #df.to_csv("insta_followers_of_" + username + ".csv", header=False)
+    #print("csv file is created.")
     driver.close()
 
 if __name__ == '__main__':
